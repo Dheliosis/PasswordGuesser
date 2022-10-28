a649efcea0795f00101b5c08c1369ee4fddc5b94
Caroline Fassot <fassot.caroline@gmail.com>
2022-10-28 14:53:53 +0200

    date class

---
+++ dateClass.py
@@ -0,0 +1,29 @@
+class Date:
+    month = {
+            "01": [1, 'janvier', 'janv'],
+            "02": [2, 'février', 'fevr'],
+            "03": [3, 'mars', 'mars'],
+            "04": [4, 'avril', 'avr'],
+            "05": [5, 'mai', 'mai'],
+            "06": [6, 'juin', 'juin'],
+            "07": [7, 'juillet', 'juill'],
+            "08": [8, 'aout', 'aout'],
+            "09": [9, 'septembre', 'sept'],
+            "10": ['octobre', 'oct'],
+            "11": ['novembre', 'nov'],
+            "12": ['decembre', 'dec'],
+        }
+    
+    def cleanDate(self, date):
+        # print('[cleanDate]')
+        date= str(date)
+        dateArray = date.split('-')
+        # print(dateArray)
+        return dateArray
+    
+    def transformMonth(self, date):
+        for el in self.month:
+            if date == el:
+                print(el)
+                print(self.month[el])
+                return self.month[el]
